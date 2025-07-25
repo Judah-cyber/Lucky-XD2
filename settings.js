70bb748aa4aa20177bd68556a9dc2fbdd81e1dc2
Judah-cyber <damilareakinyemi09@gmail.com>
2025-07-25 10:02:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~qA0m3JCD#YO-IOunbZOpyGSVppUtS9A8TGXGD8OWEXpNOtoGwZCY",
+SESSION_ID: process.env.SESSION_ID || "lucky~HBdAFaBT#AybxTUurPvLcTcuOcs627u3tbCMuKW_PnYm_y-zMJBY",
 // add your Session Id make sure it starts with lucky~
-PREFIX: process.env.PREFIX || "π",
+PREFIX: process.env.PREFIX || "*",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "JUDAH",
+BOT_NAME: process.env.BOT_NAME || "I AM NO ONE",
 // add bot name here for menu
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -68,13 +68,13 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "2347081046294",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "2348161774710",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "JUDAH",
+OWNER_NAME: process.env.OWNER_NAME || "NO ONE",
 // add bot owner name
 
-              DESCRIPTION: process.env.DESCRIPTION || "*© Powered By JUDAH*",
+              DESCRIPTION: process.env.DESCRIPTION || "*© Powered By NO ONE*",
 // add bot owner name    
 
         READ_MESSAGE: process.env.READ_MESSAGE || "false",
@@ -106,7 +106,7 @@ OWNER_NAME: process.env.OWNER_NAME || "JUDAH",
    READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
 
-     DEV: process.env.DEV || "2347081046294",
+     DEV: process.env.DEV || "2348161774710",
 //replace with your whatsapp number    
     
     ANTI_VV: process.env.ANTI_VV || "true",
