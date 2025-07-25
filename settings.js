1b9ac27106de4bbd3d0c5cc4236250fbf48261e9
Judah-cyber <damilareakinyemi09@gmail.com>
2025-07-25 09:47:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~jVsBWQDZ#l-pAIzXOhaWGI24Ya4-4KAtgFfusDhx5MVUemVscvLA",
+SESSION_ID: process.env.SESSION_ID || "lucky~qA0m3JCD#YO-IOunbZOpyGSVppUtS9A8TGXGD8OWEXpNOtoGwZCY",
 // add your Session Id make sure it starts with lucky~
-PREFIX: process.env.PREFIX || "√",
+PREFIX: process.env.PREFIX || "~",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "THE_FLASH",
+BOT_NAME: process.env.BOT_NAME || "JUDAH",
 // add bot name here for menu
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -27,7 +27,7 @@ LINK_WHITELIST: "youtube.com,github.com",
         AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 
-        AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+        AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 
         AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*Just seen ur status 😆*",
@@ -56,7 +56,7 @@ LINK_WHITELIST: "youtube.com,github.com",
 // add alive msg here 
 
 
-        STICKER_NAME: process.env.STICKER_NAME || "THE_FLASH",
+        STICKER_NAME: process.env.STICKER_NAME || "JUDAH",
 // type sticker pack name 
 
         CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
@@ -68,13 +68,13 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "2348083806476",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "2347081046294",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "THE_FLASH",
+OWNER_NAME: process.env.OWNER_NAME || "JUDAH",
 // add bot owner name
 
-              DESCRIPTION: process.env.DESCRIPTION || "*© Powered By THE FLASH*",
+              DESCRIPTION: process.env.DESCRIPTION || "*© Powered By JUDAH*",
 // add bot owner name    
 
         READ_MESSAGE: process.env.READ_MESSAGE || "false",
@@ -106,7 +106,7 @@ OWNER_NAME: process.env.OWNER_NAME || "THE_FLASH",
    READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
 
-     DEV: process.env.DEV || "2348083806476",
+     DEV: process.env.DEV || "2347081046294",
 //replace with your whatsapp number    
     
     ANTI_VV: process.env.ANTI_VV || "true",
